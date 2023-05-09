2fb49c32e64519583da610dbdc87eaef372d39d7
Muaz Yagci <m.muazyagci@gmail.com>
2023-05-09 14:30:03 +0200

    fourth day successfull

--- src/test/java/com/cambridge/Ffindingunique.java
+++ src/test/java/com/cambridge/Ffindingunique.java
@@ -2,28 +2,61 @@ package com.cambridge;
 
 import org.junit.jupiter.api.Test;
 import com.cambridge.deneme1;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Ffindingunique extends deneme1 {
+    Scanner scanner = new Scanner(System.in);
     //This class is created to avoid searching same words
-    public static ArrayList<String> word100=new ArrayList<>();
+    public static ArrayList<String> word100 = new ArrayList<>();
+
     //u1 successfully
     @Test
-    public void u1()  {
-        deneme1 deneme1=new deneme1();
-        CopyOnWriteArrayList<String> words2=new CopyOnWriteArrayList<String>();
-words2.addAll(Arrays.asList("trickling" ,
-        "solely" ,
-        "coffers",
-        "car",
-        "flash"));
+    public void u1() {
+        deneme1 deneme1 = new deneme1();
+        CopyOnWriteArrayList<String> words2 = new CopyOnWriteArrayList<String>();
+        words2.addAll(Arrays.asList("trickling",
+                "solely",
+                "coffers",
+                "car",
+                "flash",
+                "Software testing app"));
 
-        CopyOnWriteArrayList<String> words3= findingDups(words2);
+        CopyOnWriteArrayList<String> words3 = findingDups(words2);
 
         System.out.println(words3);
 
+        /*for (String each : words3) {
+            if (each.contains(" ")) {
+                System.out.println(each + " has space inside it do you want to remove it?");
+                String choise = scanner.toString();
+                if (choise.equalsIgnoreCase("Yes")){
+
+                }
+
+            }
+        }
+
+         */
+        for (int o = 0; o <1 ; o++) {
+
+
+        for (int i = 0; i < words3.size(); i++) {
+            if (words3.get(i).contains(" ")) {
+                System.out.println(words3.get(i) + " has space in it\nDo you want to remove it?Yes or No");
+                String choise = scanner.nextLine();
+                if (choise.equalsIgnoreCase("yes")) {
+                    words3.remove(i);
+                }else{
+
+                }
+            }
+        }
+            System.out.println(words3);
+        }
 
 
 
--- src/test/java/com/cambridge/deneme1.java
+++ src/test/java/com/cambridge/deneme1.java
@@ -10,56 +10,54 @@ import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class deneme1 {
-    static Scanner scanner=new Scanner(System.in);
-     ArrayList<String> words=new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+    ArrayList<String> words = new ArrayList<>();
 
     @Test
     public void first() throws InterruptedException {
 
-       // words.addAll(Arrays.asList("trickling","solely","coffers","merely","software testing craft"));
+        // words.addAll(Arrays.asList("trickling","solely","coffers","merely","software testing craft"));
         words.addAll(Arrays.asList("levied",
-                "trickling" ,
-                "solely" ,
-                "coffers" ,
-                "allotted" ,
-                "merely" ,
+                "trickling",
+                "solely",
+                "coffers",
+                "allotted",
+                "merely",
                 "attorney",
-                "fallacy" ,
+                "fallacy",
                 "thoroughly",
-                "feasible" ,
+                "feasible",
                 "trivial",
-                "İllustrating" ,
-                "Comprehensive" ,
-                "retained" ,
-                "fallible" ,
-                "inconsistent" ,
-                "propagate" ,
-                "precise" ,
-                "amplified" ,
-                "magnified" ,
-                "elusive" ,
-                "commission " ,
-                "omission" ,
-                "propagate" ,
-                "misbehave" ,
-                "essence" ,
-                "orthogonal" ,
-                "nagging" ,
+                "İllustrating",
+                "Comprehensive",
+                "retained",
+                "fallible",
+                "inconsistent",
+                "propagate",
+                "precise",
+                "amplified",
+                "magnified",
+                "elusive",
+                "commission ",
+                "omission",
+                "propagate",
+                "misbehave",
+                "essence",
+                "orthogonal",
+                "nagging",
                 "postulate"));
 
 
-
-
         System.out.println(words.size());
-        for (String each:words){
+        for (String each : words) {
             Driver.getDriver().get("https://dictionary.cambridge.org/");
 
             //Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler")).click();
-            WebElement searchBox=Driver.getDriver().findElement(By.xpath("//input[@name='q'] "));
-            searchBox.sendKeys(each+Keys.ENTER);
+            WebElement searchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q'] "));
+            searchBox.sendKeys(each + Keys.ENTER);
 
-    WebElement defBox=Driver.getDriver().findElement(By.xpath("//div[@class='def ddef_d db'] "));
-    System.out.println("\n"+each+"\n"+defBox.getText()+"\n"+Driver.getDriver().getCurrentUrl());
+            WebElement defBox = Driver.getDriver().findElement(By.xpath("//div[@class='def ddef_d db'] "));
+            System.out.println("\n" + each + "\n" + defBox.getText() + "\n" + Driver.getDriver().getCurrentUrl());
 
         }
 
@@ -72,64 +70,69 @@ public class deneme1 {
          */
 
     }
-    public  ArrayList<String> getList(){
-return words;
+
+    public ArrayList<String> getList() {
+        return words;
     }
 
-@Test
-    public void two(){
-    System.out.println(getList());
-}
+    @Test
+    public void two() {
+        System.out.println(getList());
+    }
 
-public static CopyOnWriteArrayList<String> findingDups(CopyOnWriteArrayList<String> Newwords)  {
-        ArrayList<String> Pwords=new ArrayList<>();
+    public static CopyOnWriteArrayList<String> findingDups(CopyOnWriteArrayList<String> Newwords) {
+        ArrayList<String> Pwords = new ArrayList<>();
         Pwords.addAll(Arrays.asList("levied",
-                "trickling" ,
-                "solely" ,
-                "coffers" ,
-                "allotted" ,
-                "merely" ,
+                "trickling",
+                "solely",
+                "coffers",
+                "allotted",
+                "merely",
                 "attorney",
-                "fallacy" ,
+                "fallacy",
                 "thoroughly",
-                "feasible" ,
+                "feasible",
                 "trivial",
-                "İllustrating" ,
-                "Comprehensive" ,
-                "retained" ,
-                "fallible" ,
-                "inconsistent" ,
-                "propagate" ,
-                "precise" ,
-                "amplified" ,
-                "magnified" ,
-                "elusive" ,
-                "commission " ,
-                "omission" ,
-                "propagate" ,
-                "misbehave" ,
-                "essence" ,
-                "orthogonal" ,
-                "nagging" ,
+                "İllustrating",
+                "Comprehensive",
+                "retained",
+                "fallible",
+                "inconsistent",
+                "propagate",
+                "precise",
+                "amplified",
+                "magnified",
+                "elusive",
+                "commission ",
+                "omission",
+                "propagate",
+                "misbehave",
+                "essence",
+                "orthogonal",
+                "nagging",
                 "postulate"));
-        for (String each:Pwords){
-            each=each.trim();
-            for (String Neach:Newwords){
-                    int i=0;
+        for (String each : Pwords) {
+            each = each.trim();
+            for (String Neach : Newwords) {
+                Neach = Neach.trim();
+                int i = 0;
+                /*if (Neach.contains(" ")) {
+                    Newwords.remove(i);
+                    System.out.println(Neach+" had space inside so it got removed.");
+                }
 
-                Neach=Neach.trim();
-
-try{
-    if (each.equalsIgnoreCase(Neach)){
-        System.out.println(Neach+" "+each+" are same");
-        Thread.sleep(150);
-        Newwords.remove(i);
-}
-}catch (InterruptedException e){
+                 */
 
 
 
 
+                try {
+                    if (each.equalsIgnoreCase(Neach)) {
+                        System.out.println(Neach + " " + each + " are same");
+                        Thread.sleep(150);
+                        Newwords.remove(i);
+                    }
+                } catch (InterruptedException e) {
 
                     //System.out.println(Neach+" "+each+" are same\n"+"Do you want to delete and continue?\n"+"If yes Type Yes If no Type No");
                   /*String choise1=scanner.next();
@@ -143,14 +146,15 @@ try{
                     }
 
                    */
-
+                    i++;
                 }
-                i++;
+
+
             }
         }
-    System.out.println("There you go with the unique list of words.\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
-return Newwords;
+        System.out.println("There you go with the unique list of words.\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+        return Newwords;
 
-}
+    }
 
 }
